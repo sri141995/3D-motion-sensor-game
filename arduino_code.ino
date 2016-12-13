8515a4a27bd4b70fa356d9dac057be80bf51f633
sri141995 <sridhar141995@gmail.com>
2016-12-13 16:46:18 +0530

    arduino code

---
+++ arduino_code.ino
@@ -0,0 +1,15 @@
+int trigPin=13; //Sensor Trig pin connected to Arduino pin 13
+int echoPin=11;  //Sensor Echo pin connected to Arduino pin 11
+float pingTime;  //time for ping to travel from sensor to target and return
+float targetDistance; //Distance to Target in inches
+float speedOfSound=776.5; //Speed of sound in miles per hour when temp is 77 degrees.
+ 
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+  
